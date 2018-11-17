7e54a736cfe5eda01c9a10c08b5475706800960c
Syunpon <32010151+Syunpon@users.noreply.github.com>
2018-11-17 10:31:31 +0900

    Update windowsResize.js

--- windowsResize.js
+++ windowsResize.js
@@ -4,15 +4,17 @@ var game_width_b = document.body.clientWidth;
 function ChangeCanvasSize() {
   var game_height = window.innerHeight;
   var game_width = window.innerWidth;
-
+  
   if (game_height != game_height_b || game_width != game_width_b) {
-    document.getElementById('#canvas').setAttribute("height", game_height.toString());
-    document.getElementById('#canvas').setAttribute("width", game_width.toString());
+    if (!document.getElementById('#canvas')) {
+      document.getElementById('#canvas').setAttribute("height", game_height.toString());
+      document.getElementById('#canvas').setAttribute("width", game_width.toString());
+    }
   }
-
+  
   game_height_b = game_height;
   game_width_b = game_width;
   setTimeout(ChangeCanvasSize, 100);
 }
 
-setTimeout(ChangeCanvasSize, 1000);
+ChangeCanvasSize();
