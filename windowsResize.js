f746f0534c1d9a01026317b3613407f2b7a3f919
Syunpon <32010151+Syunpon@users.noreply.github.com>
2018-11-17 10:27:32 +0900

    Update windowsResize.js

--- windowsResize.js
+++ windowsResize.js
@@ -2,13 +2,12 @@ var game_height_b = document.body.clientHeight;
 var game_width_b = document.body.clientWidth;
 
 function ChangeCanvasSize() {
-  var elem = document.getElementById('#canvas');
   var game_height = window.innerHeight;
   var game_width = window.innerWidth;
 
   if (game_height != game_height_b || game_width != game_width_b) {
-    elem.setAttribute("height", game_height.toString());
-    elem.setAttribute("width", game_width.toString());
+    document.getElementById('#canvas').setAttribute("height", game_height.toString());
+    document.getElementById('#canvas').setAttribute("width", game_width.toString());
   }
 
   game_height_b = game_height;
